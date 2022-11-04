83b2c03b92c00854fc18791b051d282562e45fbc
Liwu-di <liwudi@liwudi.fun>
2022-11-04 11:05:36 +0800

    原始代码增加数据库记录

--- src/rewrite.py
+++ src/rewrite.py
@@ -5,6 +5,7 @@
 # @Software: PyCharm
 # @Email   ：liwudi@liwudi.fun
 import argparse
+import ast
 from collections import OrderedDict
 
 import PaperCrawlerUtil.common_util
@@ -15,6 +16,7 @@ import torch.nn.functional as F
 from PaperCrawlerUtil.common_util import *
 from PaperCrawlerUtil.constant import *
 from PaperCrawlerUtil.crawler_util import *
+from PaperCrawlerUtil.research_util import *
 from dgl.nn import GATConv
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegression
@@ -22,7 +24,7 @@ from sklearn.model_selection import cross_validate
 from torch.utils.data import DataLoader
 from torch.utils.data import TensorDataset
 from torch.utils.tensorboard import SummaryWriter
-
+from params import *
 from model import *
 from utils import *
 
@@ -30,58 +32,10 @@ basic_config(logs_style=LOG_STYLE_PRINT)
 p_bar = process_bar(final_prompt="初始化准备完成", unit="part")
 p_bar.process(0, 1, 5)
 # This file implements the full version of using region embeddings to select good source data. 
-parser = argparse.ArgumentParser()
-# 源城市
-parser.add_argument('--scity', type=str, default='NY')
-# 目标城市
-parser.add_argument('--tcity', type=str, default='DC')
-# 数据集名称
-parser.add_argument('--dataname', type=str, default='Taxi', help='Within [Bike, Taxi]')
-# 数据类型
-parser.add_argument('--datatype', type=str, default='pickup', help='Within [pickup, dropoff]')
-# 尝试减小，看显存能不能撑住 32 -> 16
-parser.add_argument('--batch_size', type=int, default=16)
-# 模型
-parser.add_argument("--model", type=str, default='STNet_nobn', help='Within [STResNet, STNet, STNet_nobn]')
-# 学习率
-parser.add_argument('--learning_rate', type=float, default=1e-3)
-# 权重
-parser.add_argument('--weight_decay', type=float, default=5e-5)
-# 100回合跑下来数据有问题，改成40epoch看看，论文也是这个
-parser.add_argument('--num_epochs', type=int, default=80, help='Number of source training epochs')
-parser.add_argument('--num_tuine_epochs', type=int, default=80, help='Number of fine tuine epochs')
-# gpu设备序号
-parser.add_argument('--gpu', type=int, default=0)
-# 随机种子 不知道是干嘛的
-parser.add_argument('--seed', type=int, default=-1, help='Random seed. -1 means do not manually set. ')
-# 数据量
-parser.add_argument('--data_amount', type=int, default=0, help='0: full data, 30/7/3 correspond to days of data')
-# 内循环 源训练数量
-parser.add_argument('--sinneriter', type=int, default=3, help='Number of inner iterations (source) for meta learning')
-# 内循环 微调数量
-parser.add_argument('--tinneriter', type=int, default=1, help='Number of inner iterations (target) for meta learning')
-# 内循环元学习学习率
-parser.add_argument('--innerlr', type=float, default=5e-5, help='Learning rate for inner loop of meta-learning')
-# 外循环数量
-parser.add_argument('--outeriter', type=int, default=20, help='Number of outer iterations for meta-learning')
-# 外循环学习率
-parser.add_argument('--outerlr', type=float, default=1e-4, help='Learning rate for the outer loop of meta-learning')
-# 前k个参数
-parser.add_argument('--topk', type=int, default=15)
-# 最大平均误差参数 ，也就是beta1
-parser.add_argument('--mmd_w', type=float, default=2, help='mmd weight')
-# 边缘分类器参数， beta2
-parser.add_argument('--et_w', type=float, default=2, help='edge classifier weight')
-# 源域权重的移动平均参数
-parser.add_argument("--ma_coef", type=float, default=0.6, help='Moving average parameter for source domain weights')
-# 源域权重的正则化器。
-parser.add_argument("--weight_reg", type=float, default=1e-3, help="Regularizer for the source domain weights.")
-# 预训练回合数
-parser.add_argument("--pretrain_iter", type=int, default=-1, help='Pre-training iterations per pre-training epoch. ')
-# 预测网络学习率
-parser.add_argument("--pred_lr", type=float, default=8e-4, help="prediction learning rate")
-args = parser.parse_args()
-
+args = params()
+c = ast.literal_eval(args.c)
+record = ResearchRecord(**c)
+record_id = record.insert(__file__, get_timestamp(), args.__str__())
 if args.seed != -1:
     # seed( ) 用于指定随机数生成时所用算法开始的整数值，如果使用相同的seed( )值，则每次生成的随即数都相同，
     # 如果不设置这个值，则系统根据时间来自己选择这个值，此时每次生成的随机数因时间差异而不同。
@@ -1039,3 +993,6 @@ for ep in range(num_epochs, num_tuine_epochs + num_epochs):
     p_bar.process(0, 1, num_epochs + num_tuine_epochs)
 
 log("Best test rmse %.4f, mae %.4f" % (best_test_rmse * (max_val - min_val), best_test_mae * (max_val - min_val)))
+record.update(record_id, get_timestamp(),
+              "Best test rmse %.4f, mae %.4f" %
+              (best_test_rmse * (max_val - min_val), best_test_mae * (max_val - min_val)))
