32fd4e506f1d226e867436b47bbed394d287b1ab
Liwu-di <liwudi@liwudi.fun>
2022-11-29 16:32:12 +0800

    fix bug

--- src/time_weight.py
+++ src/time_weight.py
@@ -101,13 +101,19 @@ for i in range(source_data.shape[1]):
     for j in range(source_data.shape[2]):
         for p in range(target_data.shape[1]):
             for q in range(target_data.shape[2]):
-                time_weight1[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data[:, i, j], target_data[:, p, q])
+                if mask_source[i][j]:
+                    time_weight1[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data[:, i, j], target_data[:, p, q])
+                else:
+                    time_weight1[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = 0
             p_bar.process(0, 1, sum)
 for i in range(source_data2.shape[1]):
     for j in range(source_data2.shape[2]):
         for p in range(target_data.shape[1]):
             for q in range(target_data.shape[2]):
-                time_weight2[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data2[:, i, j], target_data[:, p, q])
+                if mask_source2:
+                    time_weight2[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data2[:, i, j], target_data[:, p, q])
+                else:
+                    time_weight2[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = 0
             p_bar.process(0, 1, sum)
 time_weight1, time_weight_max1, time_weight_min1 = min_max_normalize(time_weight1)
 time_weight2, time_weight_max2, time_weight_min2 = min_max_normalize(time_weight2)
