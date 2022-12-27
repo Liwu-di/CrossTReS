3c76d0d9d77254a5d77538b348cd9093cd97b4a1
Liwu-di <liwudi@liwudi.fun>
2022-12-27 10:21:52 +0800

    fix bug

--- src/time_weight.py
+++ src/time_weight.py
@@ -92,29 +92,61 @@ source_data2, smax2, smin2 = min_max_normalize(source_data2)
 if args.data_amount != 0:
     target_data = target_data[-args.data_amount * 24:, :, :]
 from dtaidistance import dtw
-
-time_weight1 = np.zeros((source_data.shape[1], source_data.shape[2], target_data.shape[1] * target_data.shape[2]))
-time_weight2 = np.zeros((source_data2.shape[1], source_data2.shape[2], target_data.shape[1] * target_data.shape[2]))
-sum = source_data.shape[1] * source_data.shape[2] + \
-      source_data2.shape[1] * source_data2.shape[2]
-p_bar.process(0, 1, sum)
-for i in range(source_data.shape[1]):
-    for j in range(source_data.shape[2]):
-        if mask_source[i][j]:
+bj2016 = np.load("../data/TaxiBJ16.npy")
+bj2016 = bj2016[:,:,:,0]
+bj2016_one_hour = []
+i = 0
+while i < bj2016.shape[0] - 1:
+    bj2016_one_hour.append(bj2016[i] + bj2016[i+1])
+    i = i + 2
+bj2016_one_hour = np.array(bj2016_one_hour)
+log(bj2016_one_hour.shape)
+# (20, 23)
+lng_sourcebj, lat_sourcebj = bj2016_one_hour.shape[1], bj2016_one_hour.shape[2]
+mask_sourcebj = bj2016_one_hour.sum(0) > 0
+# mask -> th_mask = (20, 23) -> (1, 20, 23)
+th_mask_sourcebj = torch.Tensor(mask_sourcebj.reshape(1, lng_sourcebj, lat_sourcebj)).to(device)
+log("%d valid regions in source" % np.sum(mask_sourcebj))
+bj2016_one_hour, bjmax, bjmin = min_max_normalize(bj2016_one_hour)
+bj_train_x, bj_train_y, bj_val_x, bj_val_y, bj_test_x, bj_test_y = split_x_y(bj2016_one_hour, lag)
+bj_x = np.concatenate([bj_train_x, bj_val_x, bj_test_x], axis=0)
+bj_y = np.concatenate([bj_train_y, bj_val_y, bj_test_y], axis=0)
+bj_dataset = TensorDataset(torch.Tensor(bj_x), torch.Tensor(bj_y))
+bj_loader = DataLoader(bj_dataset, batch_size = args.batch_size, shuffle=True)
+bj_test_dataset = TensorDataset(torch.Tensor(bj_test_x), torch.Tensor(bj_test_y))
+bj_test_loader = DataLoader(bj_test_dataset, batch_size = args.batch_size)
+time_weight_bj = np.zeros((bj2016_one_hour.shape[1], bj2016_one_hour.shape[2], target_data.shape[1] * target_data.shape[2]))
+for i in range(bj2016_one_hour.shape[1]):
+    for j in range(bj2016_one_hour.shape[2]):
+        if mask_sourcebj[i][j]:
             for p in range(target_data.shape[1]):
                 for q in range(target_data.shape[2]):
-                    time_weight1[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data[:, i, j], target_data[:, p, q])
-        p_bar.process(0, 1, sum)
-for i in range(source_data2.shape[1]):
-    for j in range(source_data2.shape[2]):
-        if mask_source2[i][j]:
-            for p in range(target_data.shape[1]):
-                for q in range(target_data.shape[2]):
-                    time_weight2[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data2[:, i, j], target_data[:, p, q])
-        p_bar.process(0, 1, sum)
-time_weight1, time_weight_max1, time_weight_min1 = min_max_normalize(time_weight1)
-time_weight2, time_weight_max2, time_weight_min2 = min_max_normalize(time_weight2)
-np.save(local_path_generate("time_weight", "time_weight{}_{}_{}_{}_{}".
-                            format(scity, tcity, datatype, dataname, args.data_amount)), time_weight1)
-np.save(local_path_generate("time_weight", "time_weight{}_{}_{}_{}_{}".
-                            format(scity2, tcity, datatype, dataname, args.data_amount)), time_weight2)
+                    time_weight_bj[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(bj2016_one_hour[:, i, j], target_data[:, p, q])
+    log("=")
+time_weight_bj, time_weight_bjmax, time_weight_bjmin = min_max_normalize(time_weight_bj)
+np.save(local_path_generate("time_weight", "time_weight{}_{}_{}_{}_{}".format("bj2016", "DC", datatype, dataname, args.data_amount)), time_weight_bj)
+# time_weight1 = np.zeros((source_data.shape[1], source_data.shape[2], target_data.shape[1] * target_data.shape[2]))
+# time_weight2 = np.zeros((source_data2.shape[1], source_data2.shape[2], target_data.shape[1] * target_data.shape[2]))
+# sum = source_data.shape[1] * source_data.shape[2] + \
+#       source_data2.shape[1] * source_data2.shape[2]
+# p_bar.process(0, 1, sum)
+# for i in range(source_data.shape[1]):
+#     for j in range(source_data.shape[2]):
+#         if mask_source[i][j]:
+#             for p in range(target_data.shape[1]):
+#                 for q in range(target_data.shape[2]):
+#                     time_weight1[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data[:, i, j], target_data[:, p, q])
+#         p_bar.process(0, 1, sum)
+# for i in range(source_data2.shape[1]):
+#     for j in range(source_data2.shape[2]):
+#         if mask_source2[i][j]:
+#             for p in range(target_data.shape[1]):
+#                 for q in range(target_data.shape[2]):
+#                     time_weight2[i][j][idx_2d_2_1d((p, q), (target_data.shape[1], target_data.shape[2]))] = dtw.distance_fast(source_data2[:, i, j], target_data[:, p, q])
+#         p_bar.process(0, 1, sum)
+# time_weight1, time_weight_max1, time_weight_min1 = min_max_normalize(time_weight1)
+# time_weight2, time_weight_max2, time_weight_min2 = min_max_normalize(time_weight2)
+# np.save(local_path_generate("time_weight", "time_weight{}_{}_{}_{}_{}".
+#                             format(scity, tcity, datatype, dataname, args.data_amount)), time_weight1)
+# np.save(local_path_generate("time_weight", "time_weight{}_{}_{}_{}_{}".
+#                             format(scity2, tcity, datatype, dataname, args.data_amount)), time_weight2)
