07a9028c2bdc9d01f22fac6ebda8bc7d136a3e54
Liwu-di <liwudi@liwudi.fun>
2023-01-15 19:19:19 +0800

    测试生成road

--- src/server_test.py
+++ src/server_test.py
@@ -758,7 +758,6 @@ for epoch in range(epochs):
 virtual_road = torch.zeros((virtual_city.shape[1] * virtual_city.shape[2], virtual_city.shape[1] * virtual_city.shape[2]))
 virtual_poi = torch.from_numpy(virtual_poi)
 virtual_poi = virtual_poi.to(device)
-virtual_poi = virtual_poi.to(torch.float64)
 for i in range(virtual_city.shape[0]):
     for j in range(virtual_city.shape[1]):
         if i >= j:
@@ -767,6 +766,7 @@ for i in range(virtual_city.shape[0]):
             m, n = idx_1d22d(i, (virtual_city.shape[1], virtual_city.shape[2]))
             p, q = idx_1d22d(j, (virtual_city.shape[1], virtual_city.shape[2]))
             dis = abs(m - p) + abs(n - q)
+            dis = torch.from_numpy([dis])
             virtual_road[i][j] = round(road_pred.forward(virtual_poi[i], virtual_poi[j], dis).item())
             virtual_road[j][i] = virtual_road[i][j]
 virtual_road = add_self_loop(virtual_road)
