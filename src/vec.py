377dc062a602105523c4bd4e4d0b5caf3b302d99
Liwu-di <liwudi@liwudi.fun>
2023-04-03 20:28:49 +0800

    de meta

--- src/vec.py
+++ src/vec.py
@@ -45,7 +45,7 @@ def generate_vector2(args):
 def generate_vector(adj, args):
     graph = nx.DiGraph(adj)
     spl = dict(nx.all_pairs_shortest_path_length(graph))
-    from .node2vec import Node2Vec as node2vec
+    from node2vec import Node2Vec as node2vec
     n2v = node2vec(G=graph, distance=spl, emb_size=args.vec_dim, length_walk=args.walk_length,
                    num_walks=args.num_walks, window_size=5, batch=4, p=args.p, q=args.q,
                    workers=(int(cpu_count() / 2)))
