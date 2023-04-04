e4e9b623106be3ab2e16864909bf5257aa780568
Liwu-di <liwudi@liwudi.fun>
2023-04-04 18:21:39 +0800

    de meta

--- src/multi_graph_merge_9.py
+++ src/multi_graph_merge_9.py
@@ -470,7 +470,7 @@ def meta_train(net_, loader_, optimizer_, weights=None, mask=None, num_iters=Non
             optimizer_.step()
             epoch_loss.append(fast_loss.item())
         else:
-            print(x.shape, y.shape, mask.shape)
+            log(x.shape, y.shape, mask.shape)
             fast_loss, fast_weights, bn_vars = net_fix(x, y, weights, mask, fast_weights,
                                                        bn_vars)
             fast_losses.append(fast_loss.item())
@@ -485,8 +485,8 @@ def net_fix(source, y, weight, mask, fast_weights, bn_vars):
         loss_source = ((pred_source - y) ** 2).view(args.meta_batch_size, 1, -1)[:, :,
                       mask.view(-1).bool()]
         loss_source = (loss_source * weight).mean(0).sum()
-    elif len(pred_source.shape) == 3:  # STNet
-        y = y.view(args.batch_size, 1, -1)[:, :, mask.view(-1).bool()]
+    elif len(pred_source.shape) == 3:# STNet
+        y = y.view(source.shape[0], 1, -1)[:, :, mask.view(-1).bool()]
         loss_source = (((pred_source - y) ** 2) * weight.view(1, 1, -1))
         loss_source = loss_source.mean(0).sum()
     fast_loss = loss_source
