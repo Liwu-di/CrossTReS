492209ce6b49e027a910b933c05322d240dd88fc
Liwu-di <liwudi@liwudi.fun>
2023-05-05 16:48:53 +0800

    de meta

--- src/multi_graph_merge_11.py
+++ src/multi_graph_merge_11.py
@@ -1646,6 +1646,7 @@ for ep in range(num_epochs):
                                            mae_s_val * (virtual_max - virtual_min),
                                            rmse_s_val + ratio * mae_s_val))
     if rmse_s_val + ratio * mae_s_val < best:
+        best = rmse_s_val + ratio * mae_s_val
         torch.save(net, root_dir_pre + "/best.pth")
     # if ((ep + 1) % 10 == 0 and ep != 0) or (ep == (num_epochs - 1)):
     #     torch.save(net, root_dir_pre + "/ep{}.pth".format(str(ep + 1)))
