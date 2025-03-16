4faab46d2b6c52db41a0e4368ed7eec4d7af2a61
liwudi <liwudi@liwudi.fun>
2025-03-16 20:48:16 +0800

    tits r2

--- src/funcs.py
+++ src/funcs.py
@@ -16,9 +16,7 @@ import scipy.stats
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from PaperCrawlerUtil.common_util import *
-from PaperCrawlerUtil.constant import *
-from PaperCrawlerUtil.crawler_util import *
+
 from dgl.nn import GATConv
 from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.linear_model import LogisticRegression
