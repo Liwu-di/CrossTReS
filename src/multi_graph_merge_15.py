5ac9134beabe74c411c603ffa5a5192ce2f59945
liwudi <liwudi@liwudi.fun>
2025-03-16 22:22:00 +0800

    tits r2

--- src/multi_graph_merge_15.py
+++ src/multi_graph_merge_15.py
@@ -1754,7 +1754,7 @@ for ep in range(num_epochs):
         if args.need_weight == 1:
             source_weights_ma = torch.ones_like(source_weights.view(-1), device=device, requires_grad=False)
         else:
-            source_weights_ma = torch.ones_like(mask_virtual.view(-1), device=device, requires_grad=False)
+            source_weights_ma = torch.ones_like(mask_virtual.reshape(-1), device=device, requires_grad=False)
     source_weights_ma = ma_param * source_weights_ma + (1 - ma_param) * source_weights
     if args.is_st_weight_static == 1:
         time_score = args.time_score_weight
